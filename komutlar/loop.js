84891f23296d90983c37966b9a5666b49b32bd2b
Zonrail <51210839+Zonrail@users.noreply.github.com>
2020-06-23 02:22:37 +0300

    Create loop.js

---
+++ komutlar/loop.js
@@ -0,0 +1,48 @@
+const { music_handler } = require("../../bot.js");
+
+module.exports.execute = async (client, message, args) => {
+
+    const queue = music_handler.getGuildQueue(message.guild.id); // Get the queue for the guild the cmd was executed in
+    if (!queue) return message.reply("⚠️ There is currently no music playing!"); // Tell the user no song is being played
+
+    if (!message.member.voice.channel || message.member.voice.channel != queue.voiceChannel) {
+        return message.reply("⚠️ You must be in the same voice channel as me to use this command!")
+    }
+
+    // Make sure the syntax of the command is correct, if not then send help message
+    if (args.length < 2) return client.commands.get("help").execute(client, message, ["loop"]);
+    if (!["song", "queue"].includes(args[0])) return client.commands.get("help").execute(client, message, ["loop"]);
+    if (!["true", "false"].includes(args[1])) return client.commands.get("help").execute(client, message, ["loop"]);
+
+    // If not boolean then send help message
+    const boolean = args[1] == "true" ? true : (args[1] == "false" ? false : args[1]);
+    if (typeof boolean != "boolean") return client.commands.get("help").execute(client, message, ["loop"]);
+
+    switch (args[0]) {
+        case "song": {
+            if (boolean == queue.videos[0].loop) return message.reply(`⚠️ Song loop is already set to ${args[1]}`);
+            queue.videos[0].loop = boolean; 
+            break;
+        }
+        case "queue": {
+            if (boolean == queue.loop) return message.reply(`⚠️ Queue loop is already set to ${args[1]}`);
+            queue.loop = boolean;
+            break;
+        }
+    }
+
+    message.channel.send(`🔃 The current \`${args[0]}\` loop has been set to \`${args[1]}\``);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["loop"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "loop",
+  description: "Tüm komutları gösterir.",
+  usage: "loop"
+};
